542fa7768165ed17e2184f6d09561b194894453d
Francisco Jimenez Cabrera <jkfran@gmail.com>
2026-03-24 23:17:16 +0000

    Remove dead code: unused includes, CSS, JS, and fonts
    Delete unused files: star rating system (no posts use ratings),
    featuredbox.html (never included), adsense include (disabled),
    toc.html (no posts use TOC), casper icon fonts (unreferenced),
    and site/ directory (leftover config).
    
    Clean up screen.css (~500 lines of dead styles), mediumish.js
    (IE hack, alertbar, spoiler handlers), lunrsearchengine.js
    (duplicate function), post/default layouts (dead conditionals),
    and _config.yml (adsense/mailchimp settings).
    
    Fix double .md.md extension on a post filename. Add .gitignore.

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,46 +1,14 @@
 jQuery(document).ready(function ($) {
-  //fix for stupid ie object cover
-  if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-    jQuery(".featured-box-img-cover").each(function () {
-      var t = jQuery(this),
-        s = "url(" + t.attr("src") + ")",
-        p = t.parent(),
-        d = jQuery("<div></div>");
-
-      p.append(d);
-      d.css({
-        height: "290",
-        "background-size": "cover",
-        "background-repeat": "no-repeat",
-        "background-position": "50% 20%",
-        "background-image": s,
-      });
-      t.hide();
-    });
-  }
-
-  // alertbar later
-  $(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 280) {
-      $(".alertbar").fadeIn();
-    } else {
-      $(".alertbar").fadeOut();
-    }
-  });
-
   // Smooth on external page
   $(function () {
     setTimeout(function () {
       if (location.hash) {
-        /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
         window.scrollTo(0, 0);
         target = location.hash.split("#");
         smoothScrollTo($("#" + target[1]));
       }
     }, 1);
 
-    // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
     $("a[href*=\\#]:not([href=\\#])").click(function () {
       if (
         location.pathname.replace(/^\//, "") ==
@@ -66,7 +34,7 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  // Hide Header on on scroll down
+  // Hide Header on scroll down
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
@@ -86,17 +54,12 @@ jQuery(document).ready(function ($) {
   function hasScrolled() {
     var st = $(this).scrollTop();
 
-    // Make sure they scroll more than delta
     if (Math.abs(lastScrollTop - st) <= delta) return;
 
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight) {
-      // Scroll Down
       $("nav").removeClass("nav-down").addClass("nav-up");
       $(".nav-up").css("top", -$("nav").outerHeight() + "px");
     } else {
-      // Scroll Up
       if (st + $(window).height() < $(document).height()) {
         $("nav").removeClass("nav-up").addClass("nav-down");
         $(".nav-up, .nav-down").css("top", "0px");
@@ -107,11 +70,6 @@ jQuery(document).ready(function ($) {
   }
 
   $(".site-content").css("margin-top", $("header").outerHeight() + "px");
-
-  // spoilers
-  $(document).on("click", ".spoiler", function () {
-    $(this).removeClass("spoiler");
-  });
 });
 
 // deferred style loading
