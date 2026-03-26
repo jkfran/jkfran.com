648afb872659f102cdf0b0ec29b716797bec0531
Francisco Jimenez Cabrera <jkfran@gmail.com>
2026-03-26 00:41:54 +0000

    Add SRI hash to Bootstrap Icons CDN, remove dead data-dismiss attributes

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -36,7 +36,7 @@ function lunr_search(term) {
     resultsEl.style.display = 'block';
     document.body.classList.add('modal-open');
 
-    resultsEl.innerHTML = '<div id="resultsmodal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul> </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    resultsEl.innerHTML = '<div id="resultsmodal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" id="btnx" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul> </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm">Close</button></div></div> </div></div>';
 
     if (term) {
         document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
